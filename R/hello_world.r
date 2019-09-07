baa09b34c6b780e89a383616a9fc5193d308a0c6
Thomas Rea <thomasmarkrea@gmail.com>
2019-09-07 12:14:13 +0100

    Documentation

--- R/hello_world.r
+++ R/hello_world.r
@@ -1,5 +1,12 @@
 # R/hello_world.r
 
+#' Hello World Function
+#'
+#' @param name A word.
+#' @return A string.
+#' @examples
+#' hello_world()
+#' hello_world('Tom')
 hello_world <- function(name = 'World') {
   glue::glue('Hello {name}')
 }
